aa65656dc6cef389c82bcd64e606652b542455ea
Michael Edwards <michael@Michaels-MacBook-Air.local>
2023-04-27 15:55:58 -0400

    added portfolio homepage and link to timestamp project

--- index.js
+++ index.js
@@ -19,6 +19,10 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/timestamp", function (req, res) {
+  res.sendFile(__dirname + "/views/timestamp.html");
+});
+
 // your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
